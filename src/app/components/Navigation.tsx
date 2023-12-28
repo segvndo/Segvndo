be264c2413e0c08f7e51ba953ef372dded1bc55b
Bea Mungcal <beamungcal@hotmail.com>
2023-12-28 04:50:27 -0500

    Create navigation component

---
+++ src/app/components/Navigation.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type NavigationProps = {};
+
+const Navigation: React.FC<NavigationProps> = () => {
+
+
+
+  return (
+    <div
+      className="Navigation">
+      Segvndo
+    </div>
+  )
+};
+
+export default Navigation;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
+import Navigation from './components/Navigation'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
+      <div>
+        <Navigation />
+      </div>
     </main>
   )
-}
+};
