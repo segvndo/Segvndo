a6cb725ced4075ec1667daeba6be5bc9681d007d
Bea Mungcal <beamungcal@hotmail.com>
2023-12-29 23:58:57 -0500

    Create navigation container

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,33 +3,35 @@ import Link from 'next/link';
 
 const Navigation: React.FC = () => {
   return (
-    <nav className="flex flex-row items-center justify-center space-x-4 p-8 bg-grey-500">
-      
-      <div>
-        <Link href="/">
-          Home
-        </Link>
-      </div>
+    <div className="nav-container bg-yellow-500 p-8 w-full h-18">
+      <nav className="flex flex-row items-center justify-center space-x-4 bg-grey-500">
 
-      <div>
-        <Link href="/about">
-          About
-        </Link>
-      </div>
+        <div>
+          <Link href="/">
+            Home
+          </Link>
+        </div>
 
-      <div>
-        <Link href="/projects">
-          Projects
-        </Link>
-      </div>
+        <div>
+          <Link href="/about">
+            About
+          </Link>
+        </div>
 
-      <div>
-        <Link href="/contact">
-          Contact
-        </Link>
-      </div>
+        <div>
+          <Link href="/projects">
+            Projects
+          </Link>
+        </div>
 
-    </nav>
+        <div>
+          <Link href="/contact">
+            Contact
+          </Link>
+        </div>
+
+      </nav>
+    </div>
   );
 };
 
