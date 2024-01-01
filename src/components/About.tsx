f03b3af2d538fa3fb504c1d996c5568769e3ea02
Bea Mungcal <beamungcal@hotmail.com>
2023-12-31 20:05:06 -0500

    Create about component

---
+++ src/components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-serif">About</h1>
+      <p className="text-2xl font-serif">This is the about page</p>
+    </div>
+  );
+};
+
+export default About;
