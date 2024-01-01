506c93b02961995f08bd14b7c13b898e527d4b0b
Bea Mungcal <beamungcal@hotmail.com>
2023-12-31 20:07:43 -0500

    Add substance to about component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ const About: React.FC = () => {
   return (
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-4xl font-serif">About</h1>
-      <p className="text-2xl font-serif">This is the about page</p>
+      <p className="text-2xl font-serif">Greetings! I am an enthusiastic Full Stack Developer committed to designing websites and applications that play a pivotal role in enhancing the overall success of the product. My path in web development is characterized by an ongoing commitment to learning, and I find fulfillment in imparting my insights and knowledge to the broader development community.</p>
     </div>
   );
 };
