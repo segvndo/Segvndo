df23d6073d0ebb953c03aa93075e9a05d7ff331f
Bea Mungcal <beamungcal@hotmail.com>
2024-01-04 23:46:37 -0500

    Create projects component with initial placeholders

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Projects: React.FC = () => {
   return (
-    <div>Projects</div>
+    <>
+      <div>Projects</div>
+
+      <div>
+        <div>Project 1</div>
+        <div>Project 2</div>
+        <div>Project 3</div>
+      </div>
+    </>
   )
 }
 
