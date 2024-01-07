f0199b6c891ba91263ef0727ae7a24f7c6287159
Bea Mungcal <beamungcal@hotmail.com>
2024-01-07 01:11:02 -0500

    Use icons for nav options and add listener to menu dropdown

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Link from 'next/link';
 import SoscMedia from './SoscMedia';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faHouse, faAddressCard, faBriefcase, faCode, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image';
 import bee from '../../assets/bee.jpg';
 
@@ -16,6 +16,27 @@ const Navigation: React.FC = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  React.useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const navContainer = document.querySelector('.nav-container');
+      if (navContainer && !navContainer.contains(event.target as Node)) {
+        closeMenu();
+      }
+    };
+
+    if (isMenuOpen) {
+      window.addEventListener('click', handleClickOutside);
+    }
+
+    return () => {
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, [isMenuOpen]);
+
   return (
     <>
       <div className="nav-container">
@@ -26,48 +47,48 @@ const Navigation: React.FC = () => {
           <div className="fixed left-0 top-0 p-4 ml-4">
             <div className="relative">
               <Image src={bee} alt="Bee Logo" height={60} width={70} className="rounded-full mb-8" />
-              {/* <h1 className="absolute bottom-0 left-2 mb-2 lg:mb-0 lg:mr-4 text-xs text-bp-primary-color">I am Bea</h1> */}
+
             </div>
           </div>
 
 
           {/* Burger Icon for Small Screens */}
           <div className="lg:hidden cursor-pointer" onClick={toggleMenu}>
-            <FontAwesomeIcon icon={faBars} className="text-bp-primary-color" size="1x" />
+            <FontAwesomeIcon icon={faBars} className="text-bp-primary-color " size="1x" />
           </div>
 
           {/* Navigation Links for Larger Screens */}
           <div className={`lg:flex ${isMenuOpen ? 'block' : 'hidden'} `}>
             <div className="mb-2 lg:mb-0 lg:mr-12">
-              <Link href="/" className="hover:text-bp-secondary-color">
-                Home
-              </Link>
+              <div className="nav-option hover:text-bp-secondary-color">
+                <FontAwesomeIcon icon={faHouse} size="1x" />
+              </div>
             </div>
 
             <div className="mb-2 lg:mb-0 lg:mr-12">
-              <Link href="/About" className="hover:text-bp-secondary-color">
-                About
-              </Link>
+              <div className="nav-option hover:text-bp-secondary-color">
+                <FontAwesomeIcon icon={faAddressCard} size="1x" />
+              </div>
             </div>
 
             <div className="mb-2 lg:mb-0 lg:mr-12">
-              <Link href="/projects" className="hover:text-bp-secondary-color">
-                Projects
-              </Link>
+              <div className="nav-option hover:text-bp-secondary-color">
+                <FontAwesomeIcon icon={faBriefcase} size="1x" />
+              </div>
             </div>
 
             <div className="mb-2 lg:mb-0 lg:mr-12">
-              <Link href="/skills" className="hover:text-bp-secondary-color">
-                Skills
-              </Link>
+              <div className="nav-option hover:text-bp-secondary-color">
+                <FontAwesomeIcon icon={faCode} size="1x" />
+              </div>
             </div>
 
             <div className="mb-2 lg:mb-0 lg:mr-12">
-              <Link href="/contact" className="hover:text-bp-secondary-color">
-                Contact
-              </Link>
+              <div className="nav-option hover:text-bp-secondary-color">
+                <FontAwesomeIcon icon={faEnvelope} size="1x" />
+              </div>
             </div>
-            
+
           </div>
         </nav>
       </div>
